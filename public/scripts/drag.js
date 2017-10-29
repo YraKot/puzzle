94c02aa301ac28cd10f8ccc1dee545d139c147a2
YraKot <y.kitsera@gmail.com>
2017-10-29 23:06:52 +0200

    add drag and drop

--- public/scripts/drag.js
+++ public/scripts/drag.js
@@ -1,29 +1,145 @@
+var DragManager = new function() {
+	  /**
+	   * {
+	   *   elem - елемент на якому була зажата миша
+	   *   avatar - аватар
+	   *   downX/downY - координати на яких був mousedown
+	   *   shiftX/shiftY - кут зсуву
+	   * }
+	   */
+	var dragObject = {};
 
+	var self = this;
 
-var els = document.getElementsByClassName('draggable');
-  
-function startDrag(evt) {
-	
+	function onMouseDown(e) {
 
-	var diffX = evt.clientX - this.offsetLeft,
-		diffY = evt.clientY - this.offsetTop,
-		that = this; 
-	
+		if (e.which != 1){
+			//права кнопка нажата
+			return;
+		}
+		var elem = e.target.closest('.draggable');
+		if (!elem){ 
+			return;
+		}
+		dragObject.elem = elem;
+		//запамятати що елемент був нажатий на координатах pageX/pageY
+		dragObject.downX = e.pageX;
+		dragObject.downY = e.pageY;
 
-	function moveAlong(evt) {
-		that.style.left = (evt.clientX - diffX) + 'px';
-		that.style.top = (evt.clientY - diffY) + 'px';
+		return false;
 	}
-	
-	function stopDrag() {
-		document.removeEventListener('mousemove', moveAlong);
-		document.removeEventListener('mouseup', stopDrag);
-	}
-	
-	document.addEventListener('mouseup', stopDrag);
-	document.addEventListener('mousemove', moveAlong);
-}
 
-for (var i = 0; i < els.length; i++) {
-	els[i].addEventListener('mousedown', startDrag);
+	function onMouseMove(e) {
+		if (!dragObject.elem) return; // елемент не зажатий
+
+		if (!dragObject.avatar) { // якщо перенос не почався
+			var moveX = e.pageX - dragObject.downX;
+			var moveY = e.pageY - dragObject.downY;
+			// якщо зажата миша пересунулося недостатньо далеко
+			if (Math.abs(moveX) < 3 && Math.abs(moveY) < 3) {
+			return;
+		}
+		// початок переносу
+		dragObject.avatar = createAvatar(e); 
+		if (!dragObject.avatar) { // відміна переносу, неможливо захопити цю частину елементу
+			dragObject = {};
+			return;
+		}
+		// аватар створений успішно
+		var coords = getCoords(dragObject.avatar);
+		dragObject.shiftX = dragObject.downX - coords.left;
+		dragObject.shiftY = dragObject.downY - coords.top;
+
+		startDrag(e); // відображення початку переносу
+	}
+
+	// перенос обєкта при кожному русі миші
+	dragObject.avatar.style.left = e.pageX - dragObject.shiftX + 'px';
+	dragObject.avatar.style.top = e.pageY - dragObject.shiftY + 'px';
+
+	return false;
+	}
+
+	function onMouseUp(e) {
+		if (dragObject.avatar) { // якшо перенос працює
+			finishDrag(e);
+		}
+		// перенос або почався або закінчився
+		//очищення стану переносу
+		dragObject = {};
+	}
+
+	function finishDrag(e) {
+		var dropElem = findDroppable(e);
+
+		if (!dropElem) {
+			self.onDragCancel(dragObject);
+		} else {
+			self.onDragEnd(dragObject, dropElem);
+		}
+	}
+
+	function createAvatar(e) {
+		// запамятати старі властивості для повернення до них при відміна
+		var avatar = dragObject.elem;
+		var old = {
+			parent: avatar.parentNode,
+			nextSibling: avatar.nextSibling,
+			position: avatar.position || '',
+			left: avatar.left || '',
+			top: avatar.top || '',
+			zIndex: avatar.zIndex || ''
+		};
+		// функция для відмини переносу
+		avatar.rollback = function() {
+			old.parent.insertBefore(avatar, old.nextSibling);
+			avatar.style.position = old.position;
+			avatar.style.left = old.left;
+			avatar.style.top = old.top;
+			avatar.style.zIndex = old.zIndex
+		};
+
+		return avatar;
+	}
+
+	function startDrag(e) {
+		var avatar = dragObject.avatar;
+		// ініціалізація початку переносу
+		document.body.appendChild(avatar);
+		avatar.style.zIndex = 9999;
+		avatar.style.position = 'absolute';
+	}
+
+	function findDroppable(event) {
+		// сховати переносимий елемент
+		dragObject.avatar.hidden = true;
+		
+		var elem = document.elementFromPoint(event.clientX, event.clientY);
+		
+		dragObject.avatar.hidden = false;
+
+		if (elem == null) {
+			// якшо курсор миші за краєм вікна
+			return null;
+		}
+		return elem.closest('.droppable');
+	}
+
+	document.onmousemove = onMouseMove;
+	document.onmouseup = onMouseUp;
+	document.onmousedown = onMouseDown;
+
+	this.onDragEnd = function(dragObject, dropElem) {};
+	this.onDragCancel = function(dragObject) {};
+
+};
+
+
+function getCoords(elem) { // непрацює в -IE8-
+	var box = elem.getBoundingClientRect();
+
+	return {
+	top: box.top + pageYOffset,
+	left: box.left + pageXOffset
+	};
 }
--- public/scripts/split.js
+++ public/scripts/split.js
@@ -35,6 +35,7 @@ document.getElementById("play").onclick = function (){
 			img.onload = splitImg;
 				
 			img.src = event.target.result;
+
 			var el = document.getElementById("canvasBg");
 			el.style.backgroundImage = "url(" + img.src + ")";
 		}
@@ -131,33 +132,6 @@ document.getElementById("play").onclick = function (){
 			  elems[i].style.left = (i+1) * 20 + 'px';
 			}
 		}
-		var els = document.getElementsByClassName('draggable');
-		
-	  function startDrag(evt) {
-		  
-	  
-		  var diffX = evt.clientX - this.offsetLeft,
-			  diffY = evt.clientY - this.offsetTop,
-			  that = this; 
-		  
-	  
-		  function moveAlong(evt) {
-			  that.style.left = (evt.clientX - diffX) + 'px';
-			  that.style.top = (evt.clientY - diffY) + 'px';
-		  }
-		  
-		  function stopDrag() {
-			  document.removeEventListener('mousemove', moveAlong);
-			  document.removeEventListener('mouseup', stopDrag);
-		  }
-		  
-		  document.addEventListener('mouseup', stopDrag);
-		  document.addEventListener('mousemove', moveAlong);
-	  }
-	  
-	  for (var i = 0; i < els.length; i++) {
-		  els[i].addEventListener('mousedown', startDrag);
-	  }
 	  
 
 	}
